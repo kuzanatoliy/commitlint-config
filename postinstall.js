6465a47b26499099dd8635d668d57b482722b445
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-11-03 11:43:40 +0300

    feat: CC-7 add cjs extencions

--- postinstall.js
+++ postinstall.js
@@ -10,17 +10,24 @@ const rootDir = resolve(cwd(), '../../..');
 
 const filenames = [
   'commitlint.config.js',
+  'commitlint.config.cjs',
   '.commitlintrc.js',
+  '.commitlintrc.cjs',
   '.commitlintrc',
   '.commitlintrc.json',
   '.commitlintrc.yml',
 ];
 
+const { type } = require(resolve(rootDir, 'package.json'));
+
 if (!filenames.find((file) => existsSync(resolve(rootDir, file)))) {
   console.info('Create commitling configuration file');
   copyFileSync(
     resolve(__dirname, 'sample.js'),
-    resolve(rootDir, 'commitlint.config.js')
+    resolve(
+      rootDir,
+      type === 'module' ? 'commitlint.config.cjs' : 'commitlint.config.js'
+    )
   );
 } else {
   console.info('Commitlint configuration file exists');
